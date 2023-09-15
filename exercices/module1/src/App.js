62047ad62c94dd4c034627719d0ed913418f35cd
damso <damien.lapinski@student.vinci.be>
2023-09-15 11:37:24 +0200

    test

--- exercices/module1/src/App.js
+++ exercices/module1/src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Hello = (props) => {
+    console.log(props)
+    return (
+    <div>
+        <p>Hello {props.name} you are {props.age} years old</p>
     </div>
-  );
+)}
+
+const App = () => {
+    const france = {name: "François", age: 'François +2'}
+    return (
+        <>
+            <h1>Greetings</h1>
+            <Hello name='Migu' age={69}/>
+            <Hello name={france.name} age={france.age}/>
+        </>
+    )
 }
 
-export default App;
+export default App
